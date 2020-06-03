9b638476ca103444055ed691c50489b3fd146a11
minhluan2292 <minhluan2292@gmail.com>
2020-06-03 15:58:45 +0700

    Create ChatApp.vue

---
+++ resources/js/components/ChatApp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="chat-app">
+        <Conversation :contact="selectedContact" :messages="messages" />
+        <ContactsList :contacts="contacts"/>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selectedContact: null,
+                messages: [],
+                contacts: []
+            };
+        },
+        mounted() {
+            axios.get('/contacts')
+                .then((response) => {
+                    this.contacts = response.data;
+                });
+        }
+    }
+</script>
