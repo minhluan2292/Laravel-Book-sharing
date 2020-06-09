be371ddcc15ea9cb5e6152fdec9ac171456fe290
minhluan2292 <minhluan2292@gmail.com>
2020-06-09 09:45:10 +0700

    Update MessagesFeed.vue
    Scroll message feed to Bot

--- resources/js/components/MessagesFeed.vue
+++ resources/js/components/MessagesFeed.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="feed">
+    <div class="feed" ref="feed">
         <ul v-if="contact">
             <li v-for="message in messages" :class="`message${message.to_user == contact.id ? ' sent' : ' received'}`" :key="message.id">
                 <div class="text">
@@ -20,6 +20,24 @@
                 type: Array,
                 required: true
             }
+        },
+        methods: {
+            scrollToBottom() {
+                setTimeout(() => {
+                    this.$refs.feed.scrollTop = this.$refs.feed.scrollHeight - this.$refs.feed.clientHeight;
+
+                }, 50);
+            }
+        },
+        watch: {
+            contact(contact)
+            {   
+                this.scrollToBottom();
+            },
+            message(messages) {
+                this.scrollToBottom();
+            }
+
         }
     }
 </script>
