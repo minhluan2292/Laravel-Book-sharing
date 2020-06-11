1f1fcbed62d99294bb1541071214d54ea416790d
minhluan2292 <minhluan2292@gmail.com>
2020-06-11 16:56:14 +0700

    Update Message.php
    Add Object User to message

--- app/Message.php
+++ app/Message.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $guarded = [];
+
+    public function fromContact()
+    {
+        return $this->hasOne(User::class, 'id', 'from_user');
+    }
 }
