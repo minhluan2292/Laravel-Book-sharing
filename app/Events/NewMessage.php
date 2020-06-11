595b219fe08367ac2f0258db2269ef090ec1c6b5
minhluan2292 <minhluan2292@gmail.com>
2020-06-11 16:56:55 +0700

    Update NewMessage.php
    broadcast Message with User

--- app/Events/NewMessage.php
+++ app/Events/NewMessage.php
@@ -38,6 +38,8 @@ class NewMessage implements ShouldBroadcast
 
     public function broadcastWith() 
     {
+        $this->message->load('fromContact');
+
         return ["message" => $this->message];
     }
 }
