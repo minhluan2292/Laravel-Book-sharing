bae87e7616fbdf3b9c4df3a927067a960198a3ec
minhluan2292 <minhluan2292@gmail.com>
2020-06-11 16:57:07 +0700

    Update ContactsList.vue

--- resources/js/components/ContactsList.vue
+++ resources/js/components/ContactsList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="contacts-list">
         <ul>
-            <li v-for="(contact, index) in contacts" :key="contact.id" @click="selectContact(index, contact)" :class="{ 'selected': index == selected }">
+            <li v-for="contact in sortedContacts" :key="contact.id" @click="selectContact(contact)" :class="{ 'selected': contact == selected }">
                 <div class="avatar">
                     <img :src="contact.profile.image" :alt="contact.name" style="max-height: 50px;max-width:100px;" />
                 </div>
@@ -9,6 +9,7 @@
                     <p class="name">{{ contact.name }}</p>
                     <p class="email">{{ contact.email }}</p>
                 </div>
+                <span class="unread" v-if="contact.unread">{{ contact.unread }}</span>
             </li>
         </ul>
     </div>
@@ -24,13 +25,24 @@
         },
         data() {
             return { 
-                selected: 0
+                selected: this.contacts.length ? this.contacts[0] : null
             };
         },
         methods: {
-            selectContact(index, contact) {
-                this.selected = index;
+            selectContact(contact) {
+                this.selected = contact;
                 this.$emit('selected', contact);
+            }
+        },
+        computed: {
+            sortedContacts() {
+                return _.sortBy(this.contacts, [(contact) => {
+                    if(contact == this.selected) {
+                        return Infinity;
+                    }
+
+                    return contact.unread;
+                }]).reverse();
             }
         }
     }
