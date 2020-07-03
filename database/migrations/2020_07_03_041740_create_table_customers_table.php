d3b40e8a5550b63c56b094402676f4634325fefa
minhluan2292 <minhluan2292@gmail.com>
2020-07-03 13:43:21 +0700

    Add Customer table and Model

---
+++ database/migrations/2020_07_03_041740_create_table_customers_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('ad_id')->nullable();
+            $table->string('list_id')->nullable();
+            $table->boolean('company_ad')->default(0);
+            $table->string('phone')->nullable();
+
+            $table->string('list_time')->nullable();
+            $table->string('date')->nullable();
+            $table->string('account_id')->nullable();
+            $table->string('account_oid')->nullable();
+            $table->string('account_name')->nullable();
+            $table->text('subject')->nullable();
+            $table->integer('category')->nullable();
+            $table->integer('area')->nullable();
+            $table->string('area_name')->nullable();
+            $table->integer('region')->nullable();
+            $table->string('region_name')->nullable();
+            $table->string('type')->nullable();
+            $table->integer('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
