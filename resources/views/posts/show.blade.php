916d35d500a8cae953f9aeca5387755c96d61e66
minhluan2292 <minhluan2292@gmail.com>
2020-07-03 10:22:35 +0700

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-8">
-            <img src="/storage/{{ $post->image }}" class="w-100">
+            <img src="/public/uploads/{{ $post->image }}" class="w-100">
         </div>
         <div class="col-4">
             <div>
