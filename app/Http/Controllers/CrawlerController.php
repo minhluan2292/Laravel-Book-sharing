3ad4722556fd0e83abb152e993c6655692cdff1f
minhluan2292 <minhluan2292@gmail.com>
2020-07-03 18:08:38 +0700

    Update CrawlerController.php

--- app/Http/Controllers/CrawlerController.php
+++ app/Http/Controllers/CrawlerController.php
@@ -2,25 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class CrawlerController extends Controller
 {
     
-    function index()
+    public function index()
     {
         return view('chotot.bds');
     }
-    public function getInfoBDS()
-    {
-        // $client = new GuzzleHttp\Client();
-        // $res = $client->get('https://gateway.chotot.com/v1/public/ad-listing?region_v2=13000&cg=1000&limit=20&st=s,k&f=a&page=1');
-        // echo $res->getStatusCode(); // 200
-        // dd($res->getBody());
-        $json = json_decode(file_get_contents('https://gateway.chotot.com/v1/public/ad-listing?region_v2=13000&cg=1000&limit=20&st=s,k&f=a&page=1'), true);
-        $data = $json['ads'][0];
-        dd($data);
-        return view('chotot.bds', compact('data'));
 
+    public function get_http_response_code($url) {
+        $headers = get_headers($url);
+        return substr($headers[0], 9, 3);
+    }
+
+
+    public function addfromChotot(Request $request)
+    {
+        $dup = 0;
+        $add = 0;
+        $error = 0;
+        $page = $request['page'];
+        $topage = $request['topage'];
+        for($p=$page;$p<=$topage;$p++)
+        {
+            $limit=50;
+            $start=($p-1)*$limit;
+
+            $json = json_decode(file_get_contents("https://gateway.chotot.com/v1/public/ad-listing?region_v2=13000&cg=1000&limit=$limit&o=$start&st=s,k&page=$p"));
+            for($i=0; $i<count($json->ads); $i++) {
+                $id = $json->ads[$i]->list_id;
+                if($this->get_http_response_code("https://gateway.chotot.com/v1/public/ad-listing/$id") == "200")
+                {
+                    $customer = json_decode(file_get_contents("https://gateway.chotot.com/v1/public/ad-listing/$id"));
+                    if(Customer::wherePhone($customer->ad->phone)->count() == 0)
+                    {
+                        $add++;          
+                        Customer::create([
+                            'ad_id' => $customer->ad->ad_id,
+                            'list_id' => $customer->ad->list_id,
+                            'company_ad' => $customer->ad->company_ad,
+                            'phone' => $customer->ad->phone,
+                            'list_time' => $customer->ad->list_time,
+                            'date' => $customer->ad->date,
+                            'account_id' => $customer->ad->account_id,
+                            'account_oid' => $customer->ad->account_oid,
+                            'account_name' => $customer->ad->account_name,
+                            'subject' => $customer->ad->subject,
+                            'category' => $customer->ad->category,
+                            'area' => $customer->ad->area,
+                            'area_name' => $customer->ad->area_name,
+                            'region' => $customer->ad->region,
+                            'region_name' => $customer->ad->region_name,
+                            'type' => $customer->ad->type,
+                            'price' => $customer->ad->price
+            
+                        ]);
+                    }
+                    else
+                    {
+                        $dup++;
+                    }
+                    
+                }  
+                else
+                {
+                    $error ++;
+                }
+
+
+            } 
+        }
+        return view('chotot.bds',compact('add','dup','error'));
     }
 }
