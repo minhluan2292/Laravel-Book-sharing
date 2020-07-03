8af8d9d44f8ef1deb2f2396bf6b10e6fdd6db84c
minhluan2292 <minhluan2292@gmail.com>
2020-07-03 18:08:43 +0700

    Update bds.blade.php

--- resources/views/chotot/bds.blade.php
+++ resources/views/chotot/bds.blade.php
@@ -5,16 +5,32 @@
     <div class="row justify-content-center">
         <div class="col-sm-12">
             <div class="card">
-                <!-- <div class="card-header">Chat App</div>
-                <div class="card-body" id="app">
 
-                    <chat-app :user='@json(Auth::user())'></chat-app> 
+            <form method="POST" action="/addfrompage">
+                        @csrf
+                        <div style="display:flex;">
+                            From:  <input style="width:10%;margin-left:10px;" id="page" type="page" class="mr-3 form-control @error('page') is-invalid @enderror" name="page" value="{{ old('page') }}" required autocomplete="page" autofocus>
+                                    @error('page')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                            To:  <input style="width:10%;margin-left:10px;" id="topage" type="topage" class="form-control @error('topage') is-invalid @enderror" name="topage" value="{{ old('topage') }}" required autocomplete="topage" autofocus>
+                                    @error('topage')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                            <button type="submit" class="btn btn-primary ml-2"> ADD </button>
+                        </div>
 
-                    
-                </div> -->
-                @foreach($data as $ads)
-                {{ $ads['list_id'] }}
-                @endforeach
+            </form>
+            @isset ($add)
+                Add {{ $add }}  
+                Dup {{ $dup }} 
+                Error {{ $error }} 
+            @endisset
+                
             </div>
         </div> 
     </div>
